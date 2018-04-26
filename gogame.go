250b19a5723b01ba34ea447b0f54f64e5dbbd402
saschel88 <as@crypta.kz>
2018-04-27 00:53:03 +0600

    Описание интерфейса и начало описание метода Move

--- gogame.go
+++ gogame.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"errors"
 )
-
+type interfaceToFigurs interface {
+	 Create (n,x,y,typeFiguri int, name string) error
+	 //Delete () error
+	 //Move () error
+	// TakeKoordinate ()error
+	//testToCreate () error // Возможно стоит добавить в Crete
+	//testToMove () error // Возможно стоит добавить в Move
+}
 type kvadrat struct {
+	Name string
 	storona int		//Значение стороны квадрата. Получаем при создании
 	firstCell [2]int
 	Pole *[20][20]int
 }
 
-func (k *kvadrat) Create (n,x,y int) error {  //метод создания квалрат с дыркой
-	err:=k.testToCreate(n,x,y)
+
+
+func (k *kvadrat) Create (n,x,y, typeFiguri int, name string) error {  //метод создания квалрат с дыркой
+	err:=k.testToCreate(n,x,y,2)
 	if err!=nil {
 			return err
 		} else {
@@ -29,11 +39,12 @@ func (k *kvadrat) Create (n,x,y int) error {  //метод создания кв
 	k.storona=n
 	k.firstCell[0]=x
 	k.firstCell[1]=y
+	k.Name=name			//Временная заглушка, данная информация
 	return nil
 	}
 }
 
-func (k *kvadrat) testToCreate(n,x,y int) error { //Метод проверки возможности создания фигуры
+func (k *kvadrat) testToCreate(n,x,y, typeFiguri int) error { //Метод проверки возможности создания фигуры
 	vertical:=x+n		//положение квадрата по вертикали
 	gorizontal:=y+n		//положение квадрата по горизонтали
 	for i:=x;i<vertical ;i++  {
@@ -45,19 +56,38 @@ func (k *kvadrat) testToCreate(n,x,y int) error { //Метод проверки 
 	}
 return nil
 }
+/*func (k *kvadrat) testToMove (moveTo, moveToNCell int) error{
+	if k.firstCell[0]-moveToNCell<=0 {
+		return errors.New("Перемещение вверх невозможно, достигается верхняя граница")
+	} else {
+		for i:=k.firstCell[0]-1;i>k.firstCell[0]-moveToNCell ;i--  {
 
+		}
+	}
+}*/
+
+/*func (k *kvadrat) Move (name string, moveTo, moveToNCell int) error {
+	switch moveTo {
+	case 1:	//move to Up
+	case 2:
+	case 3:
+	case 4:
+	default:
+
+	}
+}*/
 func main()  {
-	pole:=[20][20]int{}
+	pole:=[20][20]int{} //Наше поле
 	pole[1][4]=1
-	newKvadrat:=new(kvadrat)
-	newKvadrat.Pole=&pole
+	newKvadrat:=new(kvadrat)	//Инициализация указателя на Структуру квадрата с дфркой
+	newKvadrat.Pole=&pole  //передача алреса Нашего поля в объект структуры квадрат с дыркой
 
-	err:=newKvadrat.Create(5,1,1)
+	err:=newKvadrat.Create(5,1,1, 2,"name")
 	if err!=nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(newKvadrat)
-	for _,v :=range pole{
+
+	for _,v :=range pole{	//ВЫвод НАшего поля на экран
 		fmt.Println(v)
 	}
 
